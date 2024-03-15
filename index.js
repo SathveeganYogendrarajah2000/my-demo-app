2dc0bd5cbe3c488b71fc493905d03822970a24ea
Sathveegan <sathve@gmail.com>
2024-03-15 07:44:59 -0400

    Add express and prometheus dependencies, update port in catalog-info.yaml, and update index.js to include a basic Express server and Prometheus metrics endpoint

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello from my-demo-app!');
+const express = require('express');
+const promClient = require('prom-client');
+
+const app = express();
+const port = 8080;
+
+const counter = new promClient.Counter({
+  name: 'requests_total',
+  help: 'Total number of requests',
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
+
+app.get('/', (req, res) => {
+  counter.inc();
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
